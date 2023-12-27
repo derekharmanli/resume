57a6fdf5f6d0db9b5d680b952d5cf4a84e8a9b69
Derek Harmanli <derek.harmanli@gmail.com>
2023-12-26 21:51:04 -0500

    first changes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'React Resume',
+  description: "React Resume",
 };
 
 /**
@@ -69,7 +69,7 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Derek Harmanli.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
